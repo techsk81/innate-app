10864ee62364ea0c0b2e68561167e14b50c97c6e
Shivani <shivanikapila101@gmail.com>
2021-04-19 00:36:41 -0400

    last commit

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING, {useNewUrlParser: true,
 .catch(err => console.log(`Error occured when connecting to database ${err}`));
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, ()=>{
     console.log("Web Server is running...");
 })
